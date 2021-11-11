8c3b9227f317a5402af128855d656c4c7073d16c
muhammad fredo <muhammadfredo@gmail.com>
2021-11-11 13:27:39 +0700

    v1.0.4-o
    - add menubar button duplicate ori, and clean anim layer

---
+++ MayaMenubar/FR_Tools/08_Utility/Clean_animation_layer.py
@@ -0,0 +1,13 @@
+"""
+## SCRIPT HEADER ##
+
+Created By   : Muhammad Fredo
+Email        : muhammadfredo@gmail.com
+Start Date   : 08 Nov 2021
+Info         :
+
+"""
+import FrMaya.core as fmc
+
+fmc.clean_animation_node()
+fmc.clean_anim_layer()
