90e6d19c5e738c2880e542def6eb0ddb3f38f479
muhammad fredo <muhammadfredo@gmail.com>
2021-11-21 20:37:25 +0700

    v1.0.4-s
    - add refresh ngskin2 layers
    - clean unused node

---
+++ MayaMenubar/FR_Tools/08_Utility/Clean_unused_node.py
@@ -0,0 +1,16 @@
+"""
+## SCRIPT HEADER ##
+
+Created By   : Muhammad Fredo
+Email        : muhammadfredo@gmail.com
+Start Date   : 18 Nov 2021
+Info         :
+
+"""
+import FrMaya.core as fmc
+
+
+fmc.clean_ngskin_node()
+fmc.clean_unused_node()
+fmc.clean_unknown_node()
+fmc.clean_unknown_plugins()
